f112f0c5dc7b2ac6c64c34bc323a58c3871ba500
MJCRSIS22\Notebook <mjcrsis22@gmail.com>
2018-12-20 19:23:50 -0300

    status for app

--- src/servBike/functions/bike.js
+++ src/servBike/functions/bike.js
@@ -5,22 +5,22 @@ const _fn_bikes = {
         console.log(`bikesList request`);
         try {
             var bikes = await Bike.find();
-            cb(null, bikes);
+            cb(null, { status: '', body: bikes});
 
         } catch (err) {
             console.log('dbErrorForList', err);
-            throw "Error al intentar conectarse a la base de datos, para listar los registros";
+            cb(null, { status: 'error', body: "Error al intentar conectarse a la base de datos, para listar los registros" });
         }
     },
 
     bikesGet: async (req, cb) => {
         try {
             var bike = await Bike.findById(req.id);
-            cb(null, bike);
+            cb(null, { status: '', body: bike});
 
         } catch (err) {
             console.log('dbErrorForGet', err);
-            throw "Error al intentar conectarse a la base de datos, para obtener el registro";
+            cb(null, { status: 'error', body: "Error al intentar conectarse a la base de datos, para obtener el registro" });
         }
     },
 
@@ -28,33 +28,33 @@ const _fn_bikes = {
         try {
             var bike = new Bike(req.body);
             var newDoc = await bike.save();
-            cb(null, newDoc);
+            cb(null, { status: '', body: newDoc});
 
         } catch (err) {
             console.log('dbErrorForAdd', err);
-            throw "Error al intentar conectarse a la base de datos, para agregar el registro";
+            cb(null, { status: 'error', body: "Error al intentar conectarse a la base de datos, para agregar el registro" });
         }
     },
 
     bikesEdit: async (req, cb) => {
         try {
             await Bike.update({ _id: req.body.id }, req.body);
-            cb(null, 'Bicicleta Editada');
+            cb(null, { status: '', body: 'Bicicleta Editada'});
 
         } catch (err) {
             console.log('dbErrorForEdit', err);
-            throw "Error al intentar conectarse a la base de datos, para editar el registro";
+            cb(null, { status: 'error', body: "Error al intentar conectarse a la base de datos, para editar el registro" });
         }
     },
 
     bikesDelete: async (req, cb) => {
         try {
             await Bike.deleteOne({ _id: req.id });
-            cb(null, 'Bicicleta Eliminada');
+            cb(null, { status: '', body: 'Bicicleta Eliminada'});
 
         } catch (err) {
             console.log('dbErrorForDelete', err);
-            throw "Error al intentar conectarse a la base de datos, para eliminar el registro";
+            cb(null, { status: 'error', body: "Error al intentar conectarse a la base de datos, para eliminar el registro" });
         }
     }
 };
